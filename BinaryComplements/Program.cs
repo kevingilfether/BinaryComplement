7333592cb22f2151b3269df9e0f3253d65dc4a08
Kevin Gilfether <kevingilfether@gmail.com>
2017-12-07 20:00:35 -0500

    Working draft

--- BinaryComplements/Program.cs
+++ BinaryComplements/Program.cs
@@ -9,13 +9,16 @@ namespace BinaryComplements
 			//Goal - To take a base 10 integer, find its binary counterpart
 			//negate the binary, render it in base 10, and return that sucker
 
-
+			Console.WriteLine ("Here's a magic trick! Give me a number:");
+			int input = int.Parse (Console.ReadLine ());
+			int output = Complementer (input);
+			Console.WriteLine (output);
 
 		}
 		public static int Complementer (int input)
 		{
 			string binaryInput = Convert.ToString (input, 2);
-			string binaryNegate;
+			string binaryNegate ="";
 			foreach (char character in binaryInput) 
 			{
 				if (character == '1')
@@ -23,6 +26,8 @@ namespace BinaryComplements
 				else
 					binaryNegate += '1';
 			}
+			int baseTenified = Convert.ToInt32 (binaryNegate, 10);
+			return baseTenified;
 		}
 	}
 }
